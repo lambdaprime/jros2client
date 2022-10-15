5599a5438ba63a1ffab00d25e8de143556f03c62
lambdaprime <intid@protonmail.com>
2022-10-15 18:18:25 +0000

    Adding default Subscriber QOS policy

--- jros2client/src/main/java/id/jros2client/impl/JRos2ClientImpl.java
+++ jros2client/src/main/java/id/jros2client/impl/JRos2ClientImpl.java
@@ -35,6 +35,7 @@ import pinorobotics.rtpstalk.RtpsTalkClient;
 import pinorobotics.rtpstalk.qos.DurabilityType;
 import pinorobotics.rtpstalk.qos.PublisherQosPolicy;
 import pinorobotics.rtpstalk.qos.ReliabilityType;
+import pinorobotics.rtpstalk.qos.SubscriberQosPolicy;
 
 /**
  * Main class of the library which allows to interact with ROS.
@@ -54,6 +55,17 @@ public class JRos2ClientImpl extends LazyService implements JRos2Client {
      */
     private static final PublisherQosPolicy DEFAULT_PUBLISHER_QOS =
             new PublisherQosPolicy(
+                    ReliabilityType.RELIABLE, DurabilityType.VOLATILE_DURABILITY_QOS);
+
+    /**
+     * From http://design.ros2.org/articles/qos.html:
+     *
+     * <p>In order to make the transition from ROS 1 to ROS 2, exercising a similar network behavior
+     * is desirable. By default, publishers and subscriptions are reliable in ROS 2, have volatile
+     * durability, and keep last history.
+     */
+    private static final SubscriberQosPolicy DEFAULT_SUBSCRIBER_QOS =
+            new SubscriberQosPolicy(
                     ReliabilityType.RELIABLE, DurabilityType.VOLATILE_DURABILITY_QOS);
 
     private DdsNameMapper rosNameMapper;
@@ -85,7 +97,7 @@ public class JRos2ClientImpl extends LazyService implements JRos2Client {
                         new SameThreadExecutorService(),
                         1);
         transformer.subscribe(subscriber);
-        rtpsTalkClient.subscribe(topic, messageName, transformer);
+        rtpsTalkClient.subscribe(topic, messageName, DEFAULT_SUBSCRIBER_QOS, transformer);
     }
 
     @Override
